53a06ecb8483186e0476dee46214bc36d93778b9
Toguchi Shun <n14009@n14009.it-college.local>
2014-10-14 10:12:55 +0900

    chap1 test

--- Ex1.java
+++ Ex1.java
@@ -10,11 +10,17 @@ class Ex1_0 extends Ex {
 
 class Ex1_2 extends Ex {
 	void execute(){
-		System.out.println("渡\n久\n地\n\n駿");
+		System.out.println("渡\n久\n地\n駿");
                  
 	}
 }
-
+class Ex1_3 extends Ex {
+        void execute(){
+          System.out.println("渡\n久\n地\n\n駿");
+        
+        
+        }
+}
 public class Ex1 {
 	public static void main(String[] args){
 		Ex a;
@@ -22,5 +28,7 @@ public class Ex1 {
 		a.execute();
 		a = new Ex1_2();
 		a.execute();
+                a = new Ex1_3();
+                a.execute();
 	}
 }

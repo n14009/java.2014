f5c33ba6c0d30dc84569a5499b398ef6871e3479
Toguchi shun <n14009@std.it-college.ac.jp>
2014-11-11 11:47:30 +0900

    test

--- Ex3.java
+++ Ex3.java
@@ -1,34 +1,45 @@
 class Ex3{
 
 	public String ex3_0(int n){
-
-		return "dummy";
-
-	}
+          if (n > 0){
+		return "その値は正です";
+          }        
+                return "その値は０か負です";
+        }
 
 	public int ex3_1(int n){
 		
-		return -1;
+		return Math.abs(n);
 
 	}
 
 	public String ex3_2(int a, int b){
-
-		return "dummy";
+          if (a % b ==0){
+                return "bはaの約数です";
+          }
+                return "bはaの約数ではありません";
 	}
 
 	public String ex3_3(int a, int b){
+        if(a > b){
+                return "aの方が大きいです";
+        }
+	else if(a == 0){
+                return "同じ値です";
+        }
+                return "bの方が大きいです";
 
-		return "dummy";
-
-	}
-
+        }
 	public String ex3_4(int a){
-
-		return "dummy";
-
+        if (a > 0){
+          if(a % 5 ==0){
+                return "その値は5で割り切れます";
+          } 
+		return "その値は5では割り切れません";
+          
 	}
-
+                return "正でない整数値です";
+        }
 	public String ex3_5(int a){
 
 		return "dummy";
